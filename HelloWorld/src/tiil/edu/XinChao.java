494654575265df7131c5eefc50f50518107cbd5f
HuynhminhHuy1299 <157135547+HuynhminhHuy1299@users.noreply.github.com>
2024-01-21 13:43:55 +0700

    cập nhật code
    sửa mới và thêm dòng

--- HelloWorld/src/tiil/edu/XinChao.java
+++ HelloWorld/src/tiil/edu/XinChao.java
@@ -13,8 +13,8 @@ public class XinChao {
 	 */
 	public static void main(String[] args) {
 		//
-		System.out.println("TÔI LẦN ĐẦU HỌC JAVA, VÀ ĐANG THỬ NGHIỆM QUẢN LÝ MÃ NGUỒN VỚI GIT");
-
+		System.out.println("TÔI LẦN ĐẦU HỌC Java, VÀ ĐANG THỬ NGHIỆM QUẢN LÝ MÃ NGUỒN VỚI GIT");
+		System.out.print("Tôi thêm một dòng mới, và sửa lại chứ Java ở trên");
 	}
 
 }
