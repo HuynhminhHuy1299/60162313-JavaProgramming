fad09a4fc15c8ef0531a95fe353d9014c3d3f62c
HuynhminhHuy1299 <157135547+HuynhminhHuy1299@users.noreply.github.com>
2024-01-21 13:40:20 +0700

    Ví dụ đàu tiên
    Chỉ để demo

---
+++ HelloWorld/src/tiil/edu/XinChao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//
+		System.out.println("TÔI LẦN ĐẦU HỌC JAVA, VÀ ĐANG THỬ NGHIỆM QUẢN LÝ MÃ NGUỒN VỚI GIT");
+
+	}
+
+}
