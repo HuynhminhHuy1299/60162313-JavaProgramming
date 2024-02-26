49f727804d2ba3a614eb49284f054f3e4603ef24
HuynhminhHuy1299 <157135547+HuynhminhHuy1299@users.noreply.github.com>
2024-02-26 13:28:48 +0700

    bài 1
    Cập nhật

---
+++ Lab1bai1/src/lab3/bai1.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package lab3;
+
+/**
+ * 
+ */
+import java.util.Scanner;
+
+public class bai1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhập một số nguyên dương: ");
+		int N = sc.nextInt();
+		boolean isPrime = true;
+		for (int i = 2; i < N; i++) {
+			if (N % i == 0) {
+				//Nếu N chia hết cho i, không phải số nguyên tố
+				isPrime = false;
+				break;
+			}
+		}
+		if (isPrime) {
+			System.out.println(N + " là số nguyên tố.");
+		} else {
+			System.out.println(N + " không phải là số nguyên tố.");
+		}
+	}
+
+}
