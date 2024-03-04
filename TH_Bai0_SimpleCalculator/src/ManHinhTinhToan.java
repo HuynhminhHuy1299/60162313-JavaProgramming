0454051e50854e74c7cec57323cf2131e31900bb
HuynhminhHuy1299 <157135547+HuynhminhHuy1299@users.noreply.github.com>
2024-03-04 23:47:23 +0700

    Update ManHinhTinhToan.java

--- TH_Bai0_SimpleCalculator/src/ManHinhTinhToan.java
+++ TH_Bai0_SimpleCalculator/src/ManHinhTinhToan.java
@@ -1,9 +1,4 @@
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 public class ManHinhTinhToan extends JFrame {
 
