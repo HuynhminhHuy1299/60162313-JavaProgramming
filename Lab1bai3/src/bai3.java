84e00038d43cf76022c6db1d6a2076319c97a9de
HuynhminhHuy1299 <157135547+HuynhminhHuy1299@users.noreply.github.com>
2024-03-05 23:16:01 +0700

    Lab1bai3
    cập nhật

---
+++ Lab1bai3/src/bai3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class bai3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhập cạnh: ");
+		double canh = scanner.nextDouble();
+		
+		double Thetichlapphuong = canh * canh * canh;
+		double theTich = Math.pow(canh, 3);
+		
+		System.out.println("Thể tích của khối lập phương là: " + Thetichlapphuong + theTich);
+	}
+
+}
